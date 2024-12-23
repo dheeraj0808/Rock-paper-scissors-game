e4307a9b762a14402558c3ae8e260fc6cf25ad59
dheeraj0808 <dheerajsingh1939@gmail.com>
2024-12-23 16:08:03 +0530

    rock paper scissors game completed

--- script.js
+++ script.js
@@ -2,24 +2,36 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
+const userScorePara= document.querySelector("#user-Score");
+const compScorePara= document.querySelector("#computer-Score");
 const gencompChoice = () =>{
-  const option = ["rock","paper","ciessor"];
+  const option = ["rock","paper","scissors"];
   const randIndx = Math.floor(Math.random()*3);
   return option[randIndx];
 }
-const showWinner=(userWin)=>{
+const showWinner=(userWin ,userChoice ,compChoice)=>{
   if(userWin){
+    userScore++;
+    userScorePara.innerText=userScore;
     console.log("user Win");
+    msg.innerText = `you win! Your ${userChoice} beats ${compChoice} `;
+    msg.style.backgroundColor = "green";
   }else{
-    console.log("computer win")
+    compScore++;
+    compScorePara.innerText=compScore;
+    console.log("computer win");
+    msg.innerText = `you lost. ${compChoice} beats your ${userChoice} `;
+    msg.style.backgroundColor = "red";
   }
 
 }
-
 //for draw condition 
 const drawGame = ()=>{
   console.log("game was draw");
+  msg.innerText = "Game was draw. Play Again";
+  msg.style.backgroundColor = "black";
 }
 
 const playGame = (userChoice) =>{
@@ -39,14 +51,14 @@ const playGame = (userChoice) =>{
        userWin = compChoice === "paper" ? false:true;
 
       }else if(userChoice === "paper"){
-        userWin=compChoice ==="siessors" ? false:true;
+        userWin=compChoice ==="scissors" ? false:true;
 
 
       }else{
        userWin = compChoice === "rock" ? false:true;
 
       }
-      showWinner(userWin);
+      showWinner(userWin,userChoice,compChoice);
 
     }
 
